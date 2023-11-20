a771ded094982f3dec3b0179416be26cbbbbec17
oceanariummelaka <149451834+oceanariummelaka@users.noreply.github.com>
2023-11-20 13:36:00 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 var screenshot_button = document.getElementById('screenshot_button')
 
 screenshot_button.addEventListener('click', function() {
-  var scene = document.querySelector('a-scene').components.screenshot.getCanvas('equirectangular');})
+  var scene = document.querySelector('a-scene')
+  var canvasData = scene.renderer.domElement.toDataURL('image/png')
+  window.location = canvasData
+})
