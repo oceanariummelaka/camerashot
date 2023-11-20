8f78c7235878d0f120918699ff167f287e1a579b
oceanariummelaka <149451834+oceanariummelaka@users.noreply.github.com>
2023-11-20 14:05:41 +0800

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-import 'app.css'
-var screenshot_button = document.getElementById('screenshot_button')
-
-screenshot_button.addEventListener('click', function() {
-  var scene = document.querySelector('a-scene')
-  var canvasData = scene.renderer.domElement.toDataURL('image/png')
-  window.location = canvasData
-})
